dcb2d60c399daeea3caf9a3594912477d8cf9084
Aditya Verma <adi004.zz@gmail.com>
2018-07-27 18:47:43 +0530

    feat: Added Consonant Please

---
+++ Code Wars/Consonant Please/Consonant Please.js
@@ -0,0 +1,4 @@
+const sortLetters = arr => arr.reduce((accum, c) => 
+  !curr.forEach(elem => /[aeiou]/i.test(elem) 
+    ? accum[0].push(elem.toUpperCase()) 
+    : !(/\d/.test(elem)) && accum[1].push(elem.toUpperCase())) && accum,[[], []]);
