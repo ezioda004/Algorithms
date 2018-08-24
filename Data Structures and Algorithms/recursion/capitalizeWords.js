5c673f929013c57d184881644870a19b7596e656
Aditya Verma <adi004.zz@gmail.com>
2018-08-24 06:43:48 +0530

    feat: added capitalizeWords

---
+++ Data Structures and Algorithms/recursion/capitalizeWords.js
@@ -0,0 +1,7 @@
+function capitalizeWords (arr) {
+    if (arr.length == 0) return [];
+    return [arr[0].toUpperCase()].concat(capitalizeWords(arr.slice(1)));
+  }
+  
+  // let words = ['i', 'am', 'learning', 'recursion'];
+  // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
