3848b8142196821c069e4e8831234ab7190098a8
Aditya Verma <adi004.zz@gmail.com>
2018-08-24 06:21:51 +0530

    feat: added someRecursive

---
+++ Data Structures and Algorithms/recursion/someRecursive.js
@@ -0,0 +1,13 @@
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function someRecursive(arr, cb){
+    if (arr.length == 0) return false;
+    return cb(arr[0]) ? true : someRecursive(arr.slice(1), cb);
+    
+  }
