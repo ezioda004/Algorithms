e09080cab7db936b3261896539510b3f1c6edd2c
Aditya Verma <adi004.zz@gmail.com>
2018-09-20 11:45:20 +0530

    fix: binary search

--- Data Structures and Algorithms/binary search/binary search.js
+++ Data Structures and Algorithms/binary search/binary search.js
@@ -4,10 +4,9 @@ const binarySearch = (arr, val) => {
     while (left <= right){
         let middle = Math.floor((left + right)/2);
         if (arr[middle] == val)  return console.log(middle);
-        else if (val < arr[middle]) right = right - 1;
-        else left = left + 1;
+        else if (val < arr[middle]) right = middle - 1;
+        else left = middle + 1;
     }
     return console.log(-1);
-
 };
 binarySearch(JSON.parse(process.argv[2]), +process.argv[3]);
