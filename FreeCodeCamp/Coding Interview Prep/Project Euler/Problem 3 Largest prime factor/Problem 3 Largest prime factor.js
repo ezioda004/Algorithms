5746376695a09f762a0ddb1ffcf775e1c3d8b08c
Aditya Verma <adi004.zz@gmail.com>
2018-09-25 10:27:35 +0530

    feat: added problem 3: largest prime factor

---
+++ FreeCodeCamp/Coding Interview Prep/Project Euler/Problem 3 Largest prime factor/Problem 3 Largest prime factor.js
@@ -0,0 +1,13 @@
+function largestPrimeFactor(number) {
+  let prime = 2,
+    max = 1;
+  while (prime <= number) {
+    if (number % prime == 0) {
+      max = prime;
+      number = number / prime;
+    } else prime++;
+  }
+  return max;
+}
+
+console.log(largestPrimeFactor(13195));
